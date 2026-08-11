fe412a191e2533f6dd2ec4eba160625674be3603
Igor Oliveira <igor.rs6744@gmail.com>
2026-08-11 11:06:09 -0300

    exemplos de fecth com javascript

---
+++ revisao-javascript/dom/fetch/exemplo-users/js/script.js
@@ -0,0 +1,24 @@
+function buscarUsuarios(){
+    const container = document.getElementById('posts')
+
+    container.innerHTML = 'Carregando...'
+
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+        .then(response => response.json())
+        .then(posts => {
+            container.innerHTML = ''
+            posts.forEach(post => {
+                container.innerHTML += `
+                    <div class='card'>
+                        <h2>${post.title}</h2>
+                        <p><strong>${post.body}</strong></p>
+                    </div>
+                `
+            })
+
+            .catch(error => {
+                container.innerHTML = 'Erro ao buscar por usuários';
+                console.error('Erro:' +error);
+            })
+        })
+}
